954acc308f4db02da8b720fa3666d70181b9cdcf
Stoyan Delev <s.delev@elements.nl>
2017-12-26 20:00:19 +0100

    auto-detect default language

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import './style';
 import App from './components/app';
 import { IntlProvider } from 'preact-i18n';
-import definition from './lang/bg.json';
 
+// language
+import bg from './lang/bg.json';
+import en from './lang/en.json';
+const langList = { en, bg };
+const currentLang = langList[navigator.language.substr(0, 2)] || langList.en;
+
+console.log({ currentLang });
+
+// store
 if (!window.requestIdleCallback) {
 	window.requestIdleCallback = callback => setTimeout(callback, 50);
 }
-
 import { createStore, Provider } from 'unistore/full/preact';
 const initStore = { basket: [], products: [] };
 const memoryStore = localStorage.getItem('store');
@@ -20,7 +27,7 @@ store.subscribe(wholeStore => {
 
 export default () => (
 	<Provider store={store}>
-		<IntlProvider definition={definition}>
+		<IntlProvider definition={currentLang}>
 			<App />
 		</IntlProvider>
 	</Provider>
