ab0a406337237850c5d00647c21916bdcff23a13
Stoyan Delev <s.delev@elements.nl>
2018-01-21 19:07:01 +0100

    update products

--- src/components/product-tile/index.js
+++ src/components/product-tile/index.js
@@ -1,32 +1,32 @@
-import { h, Component } from "preact";
-import { Link } from "preact-router/match";
-import { formatCurrency } from "../../../functions/helpers";
-import { buildImageUrl } from "../../utils/images";
-import "./style";
+import { h, Component } from 'preact';
+import { Link } from 'preact-router/match';
+import { formatCurrency } from '../../../functions/helpers';
+import { buildImageUrl } from '../../utils/images';
+import './style';
 
 const Product = ({ image, price, name, id }) => (
-  <Link href={"/product/" + id} class="product tile">
-    <div class="product__thumb">
-      <picture>
-        <source
-          type="image/webp"
-          srcset={buildImageUrl(image, "300", "webp")}
-        />
-        <img
-          class="product__image"
-          src={buildImageUrl(image, "300", "jpg")}
-          alt={name}
-        />
-      </picture>
-    </div>
-    <div class="product__text">
-      <h2 class="product__name">{name}</h2>
-      <p class="product__price">
-        <span class="visuallyhidden">Price:</span> {formatCurrency(price)}
-      </p>
-    </div>
-    <div class="product__more">+</div>
-  </Link>
+	<Link href={'/product/' + id} class="product tile">
+		<div class="product__thumb">
+			<picture>
+				<source
+					type="image/webp"
+					srcset={buildImageUrl(image, '300', 'webp')}
+				/>
+				<img
+					class="product__image"
+					src={buildImageUrl(image, '300', 'jpg')}
+					alt={name}
+				/>
+			</picture>
+		</div>
+		<div class="product__text">
+			<h2 class="product__name">{name}</h2>
+			<p class="product__price">
+				<span class="visuallyhidden">Price:</span> {formatCurrency(price)}
+			</p>
+		</div>
+		<div class="product__more">+</div>
+	</Link>
 );
 
 export default Product;
